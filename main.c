5e6a6c76fac25e05d4f1b060c737cfca5333cacd
Younghwan-Lee <21500515@handong.edu>
2019-04-08 22:54:27 +0900

    main.c modify

--- main.c
+++ main.c
@@ -1,22 +1,51 @@
 #include "user.h"
 #include "menu.h"
+#include <stdio.h>
 
 int main(int argc, char* argv[]) {
   LOGIN* userlist[100]; // 사용자목록 포인터 배열 (최대 100)
   int is_login=0; // 로그인 여부 (0 NO, 1 Yes)
-  int menu_id;
+  char command[50];
   if (argc != 2) {
     printf("Usage : manager <datafile>\n");
     return 0;
   }
   int count = load_file(userlist, argv[1]);
+  printf("Welcome!!\n");
+
   while(1){
-    if(is_login==0) is_login = login(userlist, count);
-    menu_id = ask_menu(is_login); // 현재 로그인 되었는지의 여부를 파라미터로 알려야 한다.
-    if(menu_id==1) join(userlist, count);
-    else if (menu_id==2) is_login = login(userlist, count);
-    else if (menu_id==3) logout(&is_login);
-    else break;
+    if(is_login == 0){
+      printf(">");
+      scanf("%s", command);
+      if(ask_command(is_login, command) == 1){
+        if(login(userlist, count) == 1)
+          is_login += 1;
+        else
+          continue;      
+      }
+      else if(ask_command(is_login, command) == 2){
+        join(userlist, count);
+        count+=1;
+      }
+      else if(ask_command(is_login, command) == 3){
+        list(userlist, count);
+      }
+      else if(ask_command(is_login, command) == 4){
+        break;
+      }
+      else
+        printf("No such command!\n");
+    }
+    else if(is_login == 1){
+      printf("#");
+      scanf("%s", command);
+      if(ask_command(is_login, command) == 6){
+        printf("Bye!\n");
+        is_login -= 1;
+      }
+      else
+        printf("%s\n", command);
+    }
   }
   save_file(userlist, count, argv[1]);
   return 0;
